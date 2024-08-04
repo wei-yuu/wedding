7e7007fa1ef8e68c7678fb14c30be53e022c6e32
Wilson Huang <bussiness.weichen@gmail.com>
2024-08-04 16:52:18 +0800

    feature: new pages & layout

---
+++ src/router/routes.ts
@@ -0,0 +1,32 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: async () => await import('@/layout/default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: async () => await import('@/pages/index.vue'),
+      },
+      {
+        path: 'our-story',
+        name: 'story',
+        component: async () => await import('@/pages/story.vue'),
+      },
+      {
+        path: 'form',
+        name: 'form',
+        component: async () => await import('@/pages/form.vue'),
+      },
+      {
+        path: 'event-detail',
+        name: 'detail',
+        component: async () => await import('@/pages/detail.vue'),
+      },
+    ],
+  },
+];
+
+export default routes;
