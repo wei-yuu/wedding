b7377bb13ebf5b01a657807f41af16314358ad7d
Wilson Huang <bussiness.weichen@gmail.com>
2024-08-09 18:26:15 +0800

    feature: update form field style

--- src/components/form/field.vue
+++ src/components/form/field.vue
@@ -7,7 +7,9 @@
         <span class="font-chenYu text-4xl">{{ question }}</span>
       </slot>
     </span>
-    <div class="flex h-[90%] w-4/5 flex-wrap items-center justify-evenly">
+    <div
+      class="flex max-h-[90%] w-4/5 flex-wrap items-center justify-evenly py-8"
+    >
       <slot>
         <slot name="content" :data="data"></slot>
       </slot>
