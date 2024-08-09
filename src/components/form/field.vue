33b526d62c64f32d858485fb86bbe14783e67129
Wilson Huang <bussiness.weichen@gmail.com>
2024-08-09 17:31:13 +0800

    feature: update form field component

---
+++ src/components/form/field.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="relative flex h-[calc(100vh-117px)] w-full flex-col flex-wrap items-center justify-center"
+  >
+    <span class="w-full text-center">
+      <slot name="question">
+        <span class="font-chenYu text-4xl">{{ question }}</span>
+      </slot>
+    </span>
+    <div class="flex h-[90%] w-4/5 flex-wrap items-center justify-evenly">
+      <slot name="content" :data="data">
+        <slot></slot>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" generic="T">
+defineProps<{
+  data?: T;
+  question?: string;
+}>();
+</script>
