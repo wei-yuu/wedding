93c4dd936900b5245a255ef793ef33cc43a66a3e
Wilson Huang <bussiness.weichen@gmail.com>
2024-08-13 17:29:25 +0800

    feature: new option index

---
+++ src/components/form/question/index.ts
@@ -0,0 +1,23 @@
+import attend from '@/components/form/question/attend.vue';
+import attendance from '@/components/form/question/attendance.vue';
+import childSeat from '@/components/form/question/child-seat.vue';
+import eatingHabits from '@/components/form/question/eating-habits.vue';
+import friendship from '@/components/form/question/friendship.vue';
+import invitationType from '@/components/form/question/invitation-type.vue';
+import message from '@/components/form/question/message.vue';
+import name from '@/components/form/question/name.vue';
+import phone from '@/components/form/question/phone.vue';
+import relationship from '@/components/form/question/relationship.vue';
+
+export {
+  attend,
+  attendance,
+  childSeat,
+  eatingHabits,
+  friendship,
+  invitationType,
+  message,
+  name,
+  phone,
+  relationship,
+};
