360017baf8521bf73eebf95c5ce1757e5d937e67
Wilson Huang <bussiness.weichen@gmail.com>
2024-08-14 11:09:48 +0800

    config: update tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +15,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
