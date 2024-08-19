865ef8b2c976b95545f82f62741a6172519acb59
Wilson Huang <bussiness.weichen@gmail.com>
2024-08-19 11:44:15 +0800

    feature: new form-store & update form component

--- src/pages/form.vue
+++ src/pages/form.vue
@@ -1,5 +1,68 @@
 <template>
-  <div></div>
+  <div class="flex items-center justify-evenly">
+    <button
+      class="z-10 font-icon text-5xl"
+      :class="{
+        invisible: store.step === 0,
+      }"
+      @click="store.previous"
+    >
+      chevron_left
+    </button>
+    <transition
+      mode="out-in"
+      enterActiveClass="transition-all ease duration-500"
+      leaveActiveClass="transition-all ease duration-700"
+      :enterFromClass="`opacity-0 ${enterFrom}`"
+      :leaveToClass="`opacity-0 ${leaveTo}`"
+    >
+      <component
+        v-model="form[store.current]"
+        :is="formComponent[store.current]"
+        :form="store.form"
+      />
+    </transition>
+    <button
+      class="z-10 font-icon text-5xl"
+      :class="{
+        'pointer-events-none text-gray-300': !form[store.current],
+        invisible: store.step === store.tail,
+      }"
+      @click="store.next"
+    >
+      chevron_right
+    </button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as formComponent from '@/components/form/question';
+import { useFormStore } from '@/stores/form-store';
+import { computed } from 'vue';
+
+const store = useFormStore();
+
+const form = computed(() => {
+  return store.form;
+});
+
+const enterFrom = computed(() => {
+  switch (store.action) {
+    case 'previous':
+      return 'translate-x-[-100%]';
+    case 'next':
+    default:
+      return 'translate-x-full';
+  }
+});
+
+const leaveTo = computed(() => {
+  switch (store.action) {
+    case 'previous':
+      return 'translate-x-full';
+    case 'next':
+    default:
+      return 'translate-x-[-100%]';
+  }
+});
+</script>
---
+++ src/stores/form-store.ts
@@ -0,0 +1,69 @@
+import type { Form } from '@/types/store/form.type';
+import { defineStore } from 'pinia';
+import { computed, readonly, ref } from 'vue';
+
+export const useFormStore = defineStore('form', () => {
+  const form = ref<Form>({
+    name: '',
+    phone: '',
+    friendship: '',
+    relationship: '',
+    attend: '',
+    attendance: '',
+    childSeat: {
+      value: '',
+    },
+    eatingHabits: '',
+    invitationType: {
+      value: '',
+      address: '',
+      email: '',
+    },
+    message: '',
+  });
+  const action = ref('next');
+  const step = ref(0);
+  const tail = ref(Object.keys(form.value).length - 1);
+
+  const current = computed(() => {
+    const formNames = ref(Object.keys(form.value));
+
+    return formNames.value[step.value] as keyof Form;
+  });
+
+  const previous = () => {
+    action.value = 'previous';
+
+    if (
+      current.value === 'invitationType' &&
+      (form.value.attend === '禮到人不到' || form.value.attend === '否')
+    ) {
+      step.value -= 4;
+    } else {
+      step.value -= 1;
+    }
+  };
+
+  const next = () => {
+    action.value = 'next';
+
+    if (
+      current.value === 'attend' &&
+      (form.value.attend === '禮到人不到' || form.value.attend === '否')
+    ) {
+      step.value += 4;
+    } else {
+      step.value += 1;
+    }
+  };
+
+  return {
+    form,
+    action: readonly(action),
+    current: readonly(current),
+    tail: readonly(tail),
+    step: readonly(step),
+    previous,
+    next,
+  };
+});
