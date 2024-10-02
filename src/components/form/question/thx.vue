8e65e0453167f5f1b2fcdfb2f77a41ead74ba8bb
Wilson Huang <bussiness.weichen@gmail.com>
2024-10-02 22:36:12 +0800

    fix: custom button style

--- src/components/form/question/thx.vue
+++ src/components/form/question/thx.vue
@@ -1,5 +1,11 @@
 <template>
-  <form-field :show-chevrons="false" question="非常感謝您的填寫！" />
+  <form-field :show-chevrons="false" question="非常感謝您的填寫！">
+    <router-link :to="'/event-detail'">
+      <button class="custom-btn btn-7">
+        <span class="text-2xl p-1">點我看資訊</span>
+      </button>
+    </router-link>
+  </form-field>
 </template>
 
 <script setup lang="ts"></script>
