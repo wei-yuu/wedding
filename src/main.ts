2e6c80e375f21776a40481e3174a490447ff8774
Wilson Huang <bussiness.weichen@gmail.com>
2024-11-01 14:38:51 +0800

    feature: loading component

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 
 import App from './App.vue';
+import appLoading from './plugins/app-loading';
 import router from './router';
 
 import '@/styles/index.scss';
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
+const app = createApp(App).use(createPinia()).use(router).use(appLoading);
 
 app.mount('#app');
