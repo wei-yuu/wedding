7539ae86b711f7dd8eae2891abdf29bbdbd49a65
Wilson Huang <bussiness.weichen@gmail.com>
2024-11-12 18:28:12 +0800

    feature: append message api

--- src/composables/message/use-create-message.ts
+++ src/composables/message/use-create-message.ts
@@ -1,22 +1,12 @@
-import appendSheetApi from '@/api/sheet/append.api';
+import appendMessageApi from '@/api/app-script/append-message.api';
 import { useApiCall } from '../use-api-call';
 
 export const useCreateMessages = () => {
   return useApiCall(
-    async (
-      values: string[],
-      options?: {
-        majorDimension?: 'ROWS' | 'COLUMNS';
-      },
-    ) => {
-      const sheetId = '1J4nMChqEgfE6XgD4C-AgTsyBGGFGkrTSrrZp0q7Ppbw';
-      const range = 'Messages!A2:B';
-
-      return await appendSheetApi({
-        sheetId,
-        range,
-        values,
-        majorDimension: options?.majorDimension,
+    async ({ name, message }: { name: string; message: string }) => {
+      return await appendMessageApi({
+        name,
+        message,
       });
     },
   );
